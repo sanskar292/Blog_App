c2ba8598ca6f2c706ca0c2dc6f916583fa9e78f9
sanskar292 <sanskarsadaphule@gmail.com>
2026-03-23 01:06:02 +0530

    add vercel url to cors config

--- backend/src/main/java/com/yellocode/some/config/WebConfig.java
+++ backend/src/main/java/com/yellocode/some/config/WebConfig.java
@@ -13,7 +13,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                            "http://localhost:5173",
+                            "https://blog-app-jet-iota.vercel.app"
+                        )
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
